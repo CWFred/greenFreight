37004df6e535b0f202c1750773c1eac2c800d8cb
Frednick Piard <pfrednick@gmail.com>
2018-03-10 20:16:24 +0000

    GPIO test file for LED strips

---
+++ gpio.js
@@ -0,0 +1,34 @@
+
+var sleep = require('sleep');
+var Gpio = require('pigpio').Gpio;
+var wpi = require('wiring-pi');
+
+wpi.wiringPiSetupGpio();
+
+var ledRed = 17;
+var ledBlue = 22;
+var redLightIntensity = 0 ;
+var blueLightIntensity = 0;
+
+wpi.softPwmCreate(ledRed, 0, 100);
+wpi.softPwmCreate(ledBlue, 0, 100);
+
+
+while (true) {
+
+for(var i=0 ; i<100 ; i++){
+	wpi.softPwmWrite(ledRed, i);
+	wpi.softPwmWrite(ledBlue, i);
+	sleep.msleep(10);
+}
+
+for(var j=100 ; j>0 ; j--){
+	wpi.softPwmWrite(ledRed, j);
+	wpi.softPwmWrite(ledBlue, j);
+	sleep.msleep(10);
+}
+
+}
+
+
+
